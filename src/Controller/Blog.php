4dfcfe4fe27b9e5c7ce38f54cf366a544f0011b9
pushkining <pushkining@ua.fm>
2020-01-14 21:42:41 +0200

    v1.1

--- src/Controller/Blog.php
+++ src/Controller/Blog.php
@@ -4,14 +4,49 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-class Home extends AbstractController
+class Blog extends AbstractController
 {
 
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="list")
      */
-    public function index()
+    public function listPosts()
     {
-        return $this->render('home.html.twig');
+        return $this->render('posts/list.html.twig');
     }
+    
+    /**
+     * @Route("/post/add", name="show", methods={"GET"})
+     */
+    public function addPost()
+    {
+        return $this->render('posts/add.html.twig');
+    }
+
+    /**
+     * @Route("/post/{id}", name="show", methods={"GET"})
+     */
+    public function showPost(int $id)
+    {
+        var_dump($id);
+        return $this->render('posts/show.html.twig');
+    }
+        
+    /**
+     * @Route("/post/save", name="save", methods={"GET"})
+     */
+    public function savePost()
+    {
+        var_dump("Post saved");die;
+    }
+    
+    /**
+     * @Route("/post/{id}/edit", name="edit", methods={"GET"})
+     */
+    public function editPost(int $id)
+    {
+        var_dump($id);
+        return $this->render('posts/edit.html.twig');
+    }
+
 }
