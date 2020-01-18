e76941b2cc502f360ba5ddac2fc62730ca1e2d32
pushkining <pushkining@ua.fm>
2020-01-18 12:42:34 +0200

    v1.2

--- src/Controller/Blog.php
+++ src/Controller/Blog.php
@@ -1,12 +1,13 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-class Blog extends AbstractController
-{
 
+
+
+class Blog extends AbstractController
+{    
     /**
      * @Route("/", name="list")
      */
@@ -15,30 +16,6 @@ class Blog extends AbstractController
         return $this->render('posts/list.html.twig');
     }
     
-    /**
-     * @Route("/post/add", name="show", methods={"GET"})
-     */
-    public function addPost()
-    {
-        return $this->render('posts/add.html.twig');
-    }
-
-    /**
-     * @Route("/post/{id}", name="show", methods={"GET"})
-     */
-    public function showPost(int $id)
-    {
-        var_dump($id);
-        return $this->render('posts/show.html.twig');
-    }
-        
-    /**
-     * @Route("/post/save", name="save", methods={"GET"})
-     */
-    public function savePost()
-    {
-        var_dump("Post saved");die;
-    }
     
     /**
      * @Route("/post/{id}/edit", name="edit", methods={"GET"})
@@ -48,5 +25,30 @@ class Blog extends AbstractController
         var_dump($id);
         return $this->render('posts/edit.html.twig');
     }
-
+    
+     /**
+     * @Route("/post/add", name="add", methods={"GET"})
+     */
+    public function addPost()
+    {
+        return $this->render('posts/add.html.twig');
+    }
+    
+     /**
+     * @Route("/post/save", name="save", methods={"POST"})
+     */
+    public function savePost()
+    {
+        var_dump($_POST); die;
+    }
+    
+    /**
+     * @Route("/post/{id}", name="show", methods={"GET"})
+     */
+    public function showPost(int $id)
+    {
+        var_dump($id);
+        return $this->render('posts/show.html.twig');
+    }
+    
 }
